39566f52821cb0c715e50ada51b55e50c1aa8997
salehloke <salehloke@gmail.com>
2023-10-18 07:19:04 +0800

    added: delete and edit features

--- api.ts
+++ api.ts
@@ -25,9 +25,9 @@ export const addTodo = async (formData): Promise<ITask> => {
   }
 };
 
-export const editTodo = async (formData): Promise<ITask> => {
+export const editTodoPUT = async (formData): Promise<ITask> => {
   try {
-    const response = await fetch(`${baseURL}/tasks/{formData.id}`, {
+    const response = await fetch(`${baseURL}/tasks/${formData.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -36,6 +36,17 @@ export const editTodo = async (formData): Promise<ITask> => {
     });
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.log("error");
+    throw new Error(error);
+  }
+};
+
+export const deleteTodo = async (id: number): Promise<void> => {
+  try {
+    await fetch(`${baseURL}/tasks/${id}`, {
+      method: "DELETE",
+    });
   } catch (error) {
     console.log("error");
     throw new Error(error);
